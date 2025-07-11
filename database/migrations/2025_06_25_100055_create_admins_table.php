505300a6390412a33ef05313746bfdf2ebcf1879
abdulrahman_204 <abdalrahmanaljomaat@gmail.com>
2025-07-11 16:03:13 +0300

    fix migrations

--- database/migrations/2025_06_25_100055_create_admins_table.php
+++ database/migrations/2025_06_25_100055_create_admins_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
-            $table->foreignId('created_by')->nullable()->constrained('users');
+            $table->foreignId('created_by')->constrained('users');
         });
     }
 
