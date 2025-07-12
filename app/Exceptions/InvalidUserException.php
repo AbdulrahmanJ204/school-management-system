b72d4c505f4b5bc04a3950e714ec012159d2c841
Zaid <zizogithub@gmail.com>
2025-07-12 17:29:52 +0300

    invalid user exception

---
+++ app/Exceptions/InvalidUserException.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Exceptions;
+
+use App\Helpers\ResponseHelper;
+use Exception;
+
+class InvalidUserException extends Exception
+{
+    protected string $message;
+    public function __construct(string $message)
+    {
+        parent::__construct();
+        $this->message = $message;
+    }
+
+    public function render()
+    {
+        return ResponseHelper::jsonResponse(
+            null,
+            $this->message,
+            400,
+            false
+        );
+    }
+}
