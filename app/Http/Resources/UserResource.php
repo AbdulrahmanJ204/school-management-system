5b2ff5dff608170acd32a10e57fad6d7e05dd6bb
Zaid <zizogithub@gmail.com>
2025-07-17 14:58:22 +0300

    date format

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -20,7 +20,7 @@ class UserResource extends JsonResource
             'role' => $this->role,
             'image' => $this->image ? asset('storage/' . $this->image) : asset('storage/user_images/default.png'),
             'last_login' => $this->when($isGetUserRoute, function () {
-                return $this->last_login ? $this->last_login : 'This user has never logged in';
+                return $this->last_login ? $this->last_login->format('Y-m-d H:i:s') : 'This user has never logged in';
             }),
 
             'role_details' => match ($this->role) {
@@ -28,15 +28,15 @@ class UserResource extends JsonResource
                      'created_by' => $this->admin?->createdBy
                          ? trim("{$this->admin->createdBy->first_name} {$this->admin->createdBy->father_name} {$this->admin->createdBy->last_name}")
                          : null,
-                     'created_at' => $this->admin?->created_at,
-                     'updated_at' => $this->admin?->updated_at,
+                     'created_at' => $this->admin?->created_at->format('Y-m-d H:i:s'),
+                     'updated_at' => $this->admin?->updated_at->format('Y-m-d H:i:s'),
                  ],
                  'teacher' => [
                      'created_by' => $this->teacher?->createdBy
                          ? trim("{$this->teacher->createdBy->first_name} {$this->teacher->createdBy->father_name} {$this->teacher->createdBy->last_name}")
                          : null,
-                     'created_at' => $this->teacher?->created_at,
-                     'updated_at' => $this->teacher?->updated_at,
+                     'created_at' => $this->teacher?->created_at->format('Y-m-d H:i:s'),
+                     'updated_at' => $this->teacher?->updated_at->format('Y-m-d H:i:s'),
                  ],
                  'student' => [
                      'grandfather' => $this->student?->grandfather,
@@ -45,15 +45,15 @@ class UserResource extends JsonResource
                      'created_by' => $this->student?->createdBy
                          ? trim("{$this->student->createdBy->first_name} {$this->student->createdBy->father_name} {$this->student->createdBy->last_name}")
                          : null,
-                     'created_at' => $this->student?->created_at,
-                     'updated_at' => $this->student?->updated_at,
+                     'created_at' => $this->student?->created_at->format('Y-m-d H:i:s'),
+                     'updated_at' => $this->student?->updated_at->format('Y-m-d H:i:s'),
                  ],
              },
 
             'devices' => $this->when($isGetUserRoute, function () {
                 return $this->devices->map(function ($device) {
                     return [
-                        'last_login' => $this->last_login,
+                        'last_login' => $this->last_login->format('Y-m-d H:i:s'),
                         'device_id'  => $device->device_id,
                         'name'       => $device->name,
                         'type'       => $device->type,
