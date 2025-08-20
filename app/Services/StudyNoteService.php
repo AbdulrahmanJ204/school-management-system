a49b13043f1422be9314dc257241be8ce599f76f
Mohamad Alaa Alshahrour <alaaalshahror@gmail.com>
2025-08-20 06:44:35 +0300

    Apply pagination to note Api's

--- app/Services/StudyNoteService.php
+++ app/Services/StudyNoteService.php
@@ -28,11 +28,14 @@ class StudyNoteService
             'subject',
         ])
             ->orderBy('id', 'desc')
-            ->paginate(15);
+            ->paginate(50);
 
         return ResponseHelper::jsonResponse(
             StudyNoteResource::collection($studyNotes),
-            __('messages.study_note.listed')
+            __('messages.study_note.listed'),
+            200,
+            true,
+            $studyNotes->lastPage()
         );
     }
 
@@ -50,11 +53,14 @@ class StudyNoteService
                 'subject',
             ])
             ->orderBy('id', 'desc')
-            ->paginate(15);
+            ->paginate(50);
 
         return ResponseHelper::jsonResponse(
             StudyNoteResource::collection($studyNotes),
-            __('messages.study_note.listed')
+            __('messages.study_note.listed'),
+            200,
+            true,
+            $studyNotes->lastPage()
         );
     }
 
