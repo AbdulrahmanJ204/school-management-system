6f1ae230f1cdafb05420437a740ebbc9fbdc4f43
Zaid <zizogithub@gmail.com>
2025-08-26 13:20:51 +0300

    quiz edits

--- app/Http/Requests/ListQuizzesRequest.php
+++ app/Http/Requests/ListQuizzesRequest.php
@@ -19,6 +19,7 @@ class ListQuizzesRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
+
     public function rules(): array
     {
         return [
